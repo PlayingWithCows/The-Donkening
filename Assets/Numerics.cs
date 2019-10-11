7ca27fc02868bb679b9f0ee7676b6feadfd50397
PlayingWithCows <wiealbern@gmail.com>
2019-10-11 15:20:33 +0200

    first

---
+++ Assets/Numerics.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Numerics : MonoBehaviour
+{
+
+    public int pacifiers;
+
+    private TMPro.TextMeshProUGUI pacifiersDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pacifiers = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddNum(int number){
+    pacifiers += number;
+    UpdateScore();
+    }
+
+    private void UpdateScore(){
+pacifiersDisplay.text = pacifiers.ToString();
+    }
+
+}
---
+++ Assets/Printer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Printer : MonoBehaviour
+{
+
+    public float printSpeed;
+    public int productionPerCycle;
+    public Numerics num;
+
+    private bool idle = true;
+
+    public Animator animator;
+
+    public int product = 0;
+
+
+private float timeSinceLastCycle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        num = GameObject.FindObjectOfType<Numerics>();
+        timeSinceLastCycle=Time.time;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    if(timeSinceLastCycle < Time.time+printSpeed && idle==true){
+        ProducePacifier();
+        timeSinceLastCycle = Time.time;
+    }    
+    }
+
+    private void ProducePacifier(){
+        idle=false;
+animator.SetTrigger(product);
+animator.speed = printSpeed;
+       
+
+    }
+    public void GrantNumeric(){
+         num.AddNum(productionPerCycle);
+         idle=true;
+
+    }
+
+}
